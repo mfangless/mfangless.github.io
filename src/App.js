6d29ae799eafc90cb3d303057e286d6630c87704
mfangless <150011587+mfangless@users.noreply.github.com>
2024-01-04 15:22:29 +0200

    Update

--- src/App.js
+++ src/App.js
@@ -2,21 +2,34 @@ import logo from "./logo.svg";
 import "./App.css";
 
 function MyButton() {
-  return <button>I'm a button</button>;
+  return <button>Button</button>;
 }
 
 function App() {
   return (
     <>
-    <div className="App">
-      <header className="App-header">
-        <h1>MFangless</h1>
-        <p>Testing</p>
-        <MyButton />
-      </header>
-    </div>
-    <p>Testing</p>
-    <p>Testing 2</p>
+      <div className="App">
+        <header className="App-header">
+          <h1>mFangless</h1> 
+        </header>
+      </div>
+      <div className="App">
+        <header className="App-middle">
+          <p>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Velit
+            aliquet sagittis id consectetur purus ut. Enim sed faucibus turpis
+            in eu mi bibendum neque. Pharetra diam sit amet nisl suscipit
+            adipiscing bibendum est. Dui sapien eget mi proin sed libero enim. A
+            condimentum vitae sapien pellentesque habitant morbi tristique.
+            Suspendisse interdum consectetur libero id faucibus nisl tincidunt.
+            Fames ac turpis egestas maecenas pharetra convallis. Lobortis
+            elementum nibh tellus molestie nunc non blandit massa enim. Volutpat
+            commodo sed egestas egestas fringilla phasellus faucibus. Lobortis
+            mattis aliquam faucibus purus in massa
+          </p>
+        </header>
+      </div>
     </>
   );
 }
